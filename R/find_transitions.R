df8de62ea326b3469a5722d6bd21e870d6b08dcf
yakushov1 <bio.yakuschov@gmail.com>
2025-11-19 19:46:15 +0300

    freq_of_transitions-добавлена функция для расчета частоты перехода заданного параметра через заданный порог в месяц

--- R/find_transitions.R
+++ R/find_transitions.R
@@ -29,8 +29,7 @@ find_transitions <- function(df, parameter, threshold){
     dplyr::mutate(
       more_then_threshold = {{parameter}} > {{threshold}}, # логический столбец, параметр больше порога? (TRUE | FALSE)
       chng = cumsum(.data$more_then_threshold != dplyr::lag(.data$more_then_threshold, def = dplyr::first(.data$more_then_threshold)))
-    ) |>
-    dplyr::select({{parameter}}, .data$chng)
+    )
 
 }
 
@@ -44,7 +43,7 @@ find_transitions <- function(df, parameter, threshold){
 #' @param parameter a character: название столбца, для которого будут вычисляться переходы через пороговое значение threshold
 #' @param threshold a numeric: пороговое значение
 #'
-#' @returns  a tibble: содержит группирующие столбцы (если есть), столбец, для которого вычислялись переходы, а также столбец Num_of_transitions - количество переходов параметра через заданное значение
+#' @returns  a tibble: содержит группирующие столбцы, столбец, для которого вычислялись переходы, а также столбец Num_of_transitions - количество переходов параметра через заданное значение
 #' @export
 #'
 #' @examples
@@ -52,7 +51,7 @@ find_transitions <- function(df, parameter, threshold){
 #' Найти переходы температур через 0 градусов
 #' num_of_transitions(dataframe, T_avg, 0)
 #'
-#' #' Найти переходы температур через 0 градусов для каждого месяца
+#' #' Найти количество переходов температур через 0 градусов
 #' dataframe |>
 #' group_by(Year, Month) |>
 #' num_of_transitions(T_avg, 0)}
@@ -60,6 +59,50 @@ num_of_transitions <-  function(df, parameter, threshold){
   df |>
     find_transitions({{parameter}}, {{threshold}}) |>
     dplyr::summarise(Num_of_transitions = max(.data$chng))
+
+}
+
+
+
+#
+#' Частота переходов среднесуточного параметра через пороговое значение (в месяц)
+#' @description
+#' Рассчитывает частоту как количество переходов параметра через заданное пороговое значение, поделенное на продолжительность месяца (в днях)
+#'
+#'
+#' @param df a tibble: датафрейм со столбцами Year, Month, Параметр (например, T_avg). (опционально Day. Необходимо использовать датафрейм суточного разрешения, то есть одно измерение параметра должно соответствовать одним суткам)
+#' @param parameter a character: название столбца - интересующего параметра
+#' @param threshold numeric: пороговое значение, через которое совершаются переходы
+#'
+#' @returns a tibble: датафрейм со столбцами:
+#' Year
+#' Month
+#' Num_of_transitions - количество переходов через заданный порог
+#' days_in_month - количество дней в месяце (рассчитывается по входным данным)
+#' Freq_of_transitions = Num_of_transitions/days_in_month  : частота переходов (переходов в сутки)
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # пример полного папйплайна
+#' read_rp5_folder('path') |>    # считать все файлы в папке
+#'     separate_date_rp5() |>    # разбить даты на отдельные столбцы
+#'     na_fill_temp_rp5() |>     # заполнить пропуски в исходных значениях температуры
+#'     aggregate_rp5('day') |>   # агрегировать данные до суточного разрешения
+#'     na_fill_sn_rp5(10) |>     # заполнить пропуски в измерениях глубин снежного покрова
+#'     freq_of_transitions(T_avg, 0)} # рассчитать частоту и количество переходов среднесуточных температур через 0
+freq_of_transitions <- function(df, parameter, threshold){
+
+  transitions <- df |>
+    dplyr::group_by(.data$Year, .data$Month) |>
+    num_of_transitions({{parameter}}, {{threshold}})
+
+  days_count <- df |>
+    dplyr::group_by(.data$Year, .data$Month) |>
+    dplyr::summarise(days_in_month = dplyr::n(), .groups = "drop")
+
+  dplyr::left_join(transitions, days_count, by = c("Year", "Month")) |>
+    dplyr::mutate(Freq_of_transitions = .data$Num_of_transitions / .data$days_in_month)
 }
 
 
