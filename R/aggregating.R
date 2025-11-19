da99a13052ebf85bb7f4b278c30146b8fcde03c5
yakushov1 <bio.yakuschov@gmail.com>
2025-11-19 16:00:21 +0300

    добавлена функция na_fill_sn_rp5 для очистки данных о снеге суточного разрешения

--- R/aggregating.R
+++ R/aggregating.R
@@ -8,9 +8,13 @@
 #'
 #'
 #' @param df a tibble: тиббл, полученный после выполнения функций \code{\link{read_rp5_csv}} или \code{\link{read_rp5_folder}} и
-#' \code{\link{separate_date_rp5}}. Должен содержать в себе столбцы: обязательно - Year, Month, Day;
-#' опционально - любой из T, RRR, sss или в любом сочетании.
-#' Для других столбцов агрегация производиться не будет, даже если они присутствуют в датафрейме.
+#' \code{\link{separate_date_rp5}}. Должен содержать в себе столбцы:
+#' Year, Month, Day - для агрегации до суточного разрешения (работа с сырыми данными);
+#' Year, Month - для агрегации до месячного разрешения (работа с предобработанными данными суточного разрешения);
+#' Year - для агрегации до годового разрешения;
+#' опционально - любой из T, RRR, sss (или T_avg, RRR_sum, sss_avg,
+#' если предварительно агрегировали данные с помощью \code{\link{aggregate_rp5}}) или в любом сочетании.
+#' Остальные столбцы учитываться не будут, даже если они присутствуют в датафрейме.
 #'
 #' @param resolution character: до какого разрешения агрегировать данные? До суточного: resolution = 'day',
 #' месячного: resolution = 'month', годового: resolution = 'year'.
@@ -22,7 +26,20 @@
 #'
 #' @examples
 #' \dontrun{
-#' aggregate_rp5(df, 'day')}
+#' # Примеры полного пайплайна:
+#' # агрегация до суточного разрешения:
+#' read_rp5_folder('path') |>
+#'   separate_date_rp5() |> # не забудьте разделить компоненты даты
+#'   na_fill_temp_rp5() |>  # опционально, но рекомендуется обработать пропуски
+#'   aggregate_rp5('day')
+#'
+#' # агрегация до месячного или годового разрешения
+#' # (рекомендуется предварительно агрегировать до суточного и обработать пропуски)
+#' read_rp5_folder('path') |>
+#'   separate_date_rp5() |> # не забудьте разделить компоненты даты
+#'   na_fill_temp_rp5() |>  # опционально, но рекомендуется обработать пропуски
+#'   aggregate_rp5('month') # можно заменить на 'year'
+#'}
 aggregate_rp5 <- function(df, resolution){
   if (!resolution %in% c('day', 'month', 'year')) {
     return("Error: Choose correct resolution for aggregating: 'year', 'month' or 'day'")
@@ -37,8 +54,10 @@ aggregate_rp5 <- function(df, resolution){
   result <- df |>
     dplyr::group_by(dplyr::across(dplyr::all_of(group_vars))) |>
     dplyr::summarise(N = dplyr::n(),
-                     dplyr::across(dplyr::any_of(c("T", "sss")), list('avg' = ~ round(mean(.x, na.rm = TRUE), 2))),
-                     dplyr::across(dplyr::any_of(c("RRR")), list('sum' = ~ round(sum(.x, na.rm = TRUE), 2))),
+                     dplyr::across(dplyr::any_of(c("T_avg", "sss_avg")), ~round(mean(.x, na.rm = TRUE), 2)),
+                     dplyr::across(dplyr::any_of(c("T", "sss")), list("avg" = ~round(mean(.x, na.rm = TRUE), 2))),
+                     dplyr::across(dplyr::any_of(c("RRR_sum")), ~round(sum(.x, na.rm = TRUE), 2)),
+                     dplyr::across(dplyr::any_of(c("RRR")), list("sum" = ~round(sum(.x, na.rm = TRUE), 2))),
                      .groups = 'drop')
 
   return(result)
--- R/na_processing.R
+++ R/na_processing.R
@@ -41,10 +41,6 @@ na_count_rp5 <- function(df){
 
 }
 
-# обычно, если пропущен RRR в необработанных данных, то это просто 0.
-# снег измеряется не каждый час, а хотя бы один раз в сутки.
-# поэтому снег лучше агрегировать до суточного значения, а потом обрабатывать пропуски
-# температура - если пропущена, то заполнять линейной интерполяцией. Имеет смысл заполнять сырые данные, потом агрегировать
 
 
 #' Заполнение пропущенных значений температур
@@ -60,10 +56,9 @@ na_count_rp5 <- function(df){
 #'
 #' @examples
 #' \dontrun{
-  #' # Подсчет пропусков в агрегированных данных
-  #' df <- read_rp5_folder('path') |>
-  #' separate_date_rp5() |>
-  #'   na_fill_temp_rp5(replace_original = F) }
+#' read_rp5_folder('path') |>
+#'       separate_date_rp5() |>
+#'       na_fill_temp_rp5(replace_original = F) }
 na_fill_temp_rp5 <- function(df, replace_original = TRUE){
   # Определяем имя столбца с температурой
   temp_col <- NULL
@@ -84,6 +79,69 @@ na_fill_temp_rp5 <- function(df, replace_original = TRUE){
     df$Temperature_interpolated <- interpolated
   }
 
+  return(df)
+
+}
+
+
+
+# обработка пропусков в  снеге (суточное разрешение)
+# логика: если снега нет долго после низкого значения, значит, он растаял (заменить на 0)
+# если между измеренными значениями промежуток небольшой, то заменить средним значением
+
+#' Обработка пропущенных значений глубины снежного покрова (только данные суточного разрешения)
+#' @description
+#' Логика работы: если значения глубины снежного покрова пропущены длительное время (заданный порог) - он растаял.
+#' Такие пропуски стоит заменить на 0.
+#' Если число подряд следующих пропусков менее порога - заменить на последнее значение перед пропусками.
+#' Работает только с датафреймом суточного разрешения! (должны быть столбцы Year, Month, Day)#'
+#'
+#' @param df a tibble: данные суточного разрешения со столбцами Year, Month, Day и sss_svg.
+#' @param threshold numeric: пороговое значение длины "окна" с пропусками
+#' @param replace_original logical: заменять ли исходный столбец? по умолчанию TRUE
+#'
+#' @returns a tibble: исходный тиббл, где пропуски в sss_avg заменены (либо добавлен новый столбец sss_replaced - удобно для диагностики)
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' read_rp5_folder('path') |>
+#'       separate_date_rp5() |>
+#'       na_fill_temp_rp5(replace_original = F) |>
+#'       aggregate_rp5('day') |>
+#'       na_fill_sn_rp5(10, replace_original = F)}
+na_fill_sn_rp5 <- function(df, threshold, replace_original = TRUE){
+
+  df <- df |>
+    dplyr::arrange(.data$Year, .data$Month, .data$Day)
+
+
+  na_position <- df |>
+    dplyr::select(.data$sss_avg) |> # только нужный столбец для экономии памяти
+    dplyr::mutate(order_number = c(1:nrow(df))) |>  # зададим всем строкам порядковые номера
+    dplyr::filter(!is.na(.data$sss_avg)) |> # оставим только непропущенные значения
+    dplyr::mutate(first_na_position = dplyr::lag(.data$order_number)+1, # позиция первого NA в серии подряд следующих пропусков
+                  last_na_position = .data$order_number - 1, # позиция последнего NA в серии подряд следующих пропусков
+                  na_count = .data$order_number - .data$first_na_position) |> # количество пропусков подряд
+    dplyr::filter(.data$na_count > {threshold}) # можно менять пороговое значение
+
+
+  indices_to_replace <- unlist(
+    purrr::map2(na_position$first_na_position, na_position$last_na_position, ~ .x:.y)
+  )
+
+  if(replace_original == TRUE){
+    df$sss_avg[indices_to_replace] <- 0 # заменить на 0 все строки, где снег был пропущен подряд более, чем threshold
+
+    df <- df |>
+      tidyr::fill(.data$sss_avg, .direction = "down") # оставшиеся пропуски заполнить предыдущим непропущенным числом
+  } else{
+    df$sss_replaced = df$sss_avg
+    df$sss_replaced[indices_to_replace] <- 0
+
+    df <- df |>
+      tidyr::fill(.data$sss_replaced, .direction = "down")
+  }
   return(df)
 
 }
